8687afef412c6eb255d2da3d4ab4849d60bfd080
Diego <drbv27@gmail.com>
2021-11-23 06:59:31 -0500

    Conexion a ATLAS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
-const app = express();
-const port = 3000;
 
+const indexRoutes = require("./routes/index.routes");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3005;
+
+//conexion a ATLAS
+const mongoose = require("mongoose");
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.mzxff.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("Atlas conectado"))
+  .catch((e) => console.log(e));
+
+app.use("/", indexRoutes);
 app.listen(port, () => {
   console.log("Servidor corriendo en el puerto", port);
 });
